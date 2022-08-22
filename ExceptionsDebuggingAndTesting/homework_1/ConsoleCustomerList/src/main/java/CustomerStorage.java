3a0f779b72858d128a608bdda69c8dea8ec3d163
aleksei_abramov_2 <CQMZ4zCJWWpDYqz>
2022-08-22 23:23:29 +0600

    праетическая работа 14.10 задание 1

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,11 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
+    private Logger logger;
 
     public CustomerStorage() {
         storage = new HashMap<>();
+        logger = LogManager.getRootLogger();
     }
 
     public void addCustomer(String data) {
@@ -15,15 +20,47 @@ public class CustomerStorage {
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+        String regexEmail1 = "[AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz.]+@[AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz.]+\\.com";
+        String regexEmail2 = "[AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz.]+@[AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz.]+\\.ru";
+        String regexPhone = "\\+7[0-9]{10}";
+        String massage ="Wrong command! Available command examples: \n" +
+                "add Василий Петров vasily.petrov@gmail.com +79215637722";
+        if (components.length != 4) {
+            logger.error("IllegalArgumentException: array length: " + components.length + ". Expected: 4");
+            throw new IllegalArgumentException(massage);
+        }
+        if (!components[INDEX_EMAIL].matches(regexEmail1) && !components[INDEX_EMAIL].matches(regexEmail2)) {
+            logger.error("Wrong format email");
+            throw new IllegalArgumentException(massage);
+        }
+        if (!components[INDEX_PHONE].matches(regexPhone)) {
+            logger.error("Wrong format phone");
+            throw new IllegalArgumentException(massage);
+        }
+
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
     public void listCustomers() {
+        if (storage.isEmpty()) {
+            logger.error("List is empty");
+            throw new IllegalArgumentException("List is empty");
+        }
         storage.values().forEach(System.out::println);
     }
 
     public void removeCustomer(String name) {
+        String message = "Wrong command! Available command examples: \nremove Василий Петров";
+        String[] components = name.split("\\s+");
+        if (components.length != 2){
+            logger.error("IllegalArgumentException: array length: " + components.length + ". Expected: 2");
+            throw new IllegalArgumentException(message);
+        }
+        if (!storage.containsKey(name)) {
+            logger.error("IllegalArgumentException. Customer: \"" + name + "\" not found");
+            throw new IllegalArgumentException("Customer: \"" + name + "\" not found");
+        }
         storage.remove(name);
     }
 
--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/Main.java
@@ -1,6 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
 public class Main {
+    private static Logger logger;
     private static final String ADD_COMMAND = "add Василий Петров " +
             "vasily.petrov@gmail.com +79215637722";
     private static final String COMMAND_EXAMPLES = "\t" + ADD_COMMAND + "\n" +
@@ -12,23 +16,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
+        logger = LogManager.getRootLogger();
 
         while (true) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(COMMAND_ERROR);
+                String command = scanner.nextLine();
+                logger.info("User input: " + command);
+                String[] tokens = command.split("\\s+", 2);
+                String messageError = "IllegalArgumentException.User input: \"" + command + "\"";
+            try {
+                if (tokens[0].equals("add")) {
+                    if (tokens.length == 1) {
+                        logger.error(messageError);
+                        throw new IllegalArgumentException(COMMAND_ERROR);
+                    }
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    if (tokens.length == 1) {
+                        logger.error(messageError);
+                        throw new IllegalArgumentException(COMMAND_ERROR);
+                    }
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(COMMAND_ERROR);
+                }
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
             }
         }
     }
