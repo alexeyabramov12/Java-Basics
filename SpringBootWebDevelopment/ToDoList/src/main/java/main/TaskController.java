fb71a545d093e4e661be8788a67c1053885b87bc
aleksei_abramov_2 <CQMZ4zCJWWpDYqz>
2022-11-18 21:46:19 +0600

    created ToDoList

---
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/TaskController.java
@@ -0,0 +1,80 @@
+package main;
+
+
+import main.model.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import main.model.Task;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@RestController
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping("/tasks/")
+    public ResponseEntity<Task> add (Task task) {
+        if (task == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        task.setCreationTime(LocalDateTime.now());
+        task.setDone("false");
+        Task saveTask = taskRepository.save(task);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saveTask);
+    }
+
+    @GetMapping("/tasks/")
+    public List<Task> list() {
+        return StreamSupport.stream(taskRepository.findAll().spliterator(), false).collect(Collectors.toList());
+    }
+
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<Task> get(@PathVariable int id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(optionalTask.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    private ResponseEntity<Task> delete(@PathVariable int id) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            taskRepository.delete(optionalTask.get());
+            return ResponseEntity.status(HttpStatus.OK).body(optionalTask.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+
+    @PatchMapping("/tasks/{id}")
+    private ResponseEntity<Task> update(@PathVariable int id,
+                                        @RequestParam(required = false) Boolean isDone,
+                                        @RequestParam(required = false) String title,
+                                        @RequestParam(required = false) String description) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (optionalTask.isPresent()) {
+            Task updateTask = optionalTask.get();
+            if (isDone != null ) {
+                updateTask.setDone(isDone.toString());
+            }
+            if (title != null && !title.isEmpty() && !title.equals(updateTask.getTitle())) {
+                updateTask.setTitle(title);
+            }
+            if (description != null && !description.isEmpty() && !description.equals(updateTask.getDescription())) {
+                updateTask.setDescription(description);
+            }
+            taskRepository.save(updateTask);
+            return ResponseEntity.status(HttpStatus.CREATED).body(updateTask);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
+}
---
+++ SpringBootWebDevelopment/ToDoList/src/main/java/main/model/Task.java
@@ -0,0 +1,59 @@
+package main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private LocalDateTime creationTime;
+    private String isDone;
+    private String title;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(LocalDateTime creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public String isDone() {
+        return isDone;
+    }
+
+    public void setDone(String done) {
+        isDone = done;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
