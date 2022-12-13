0c74f942305ad44aa19526aae25145f33d839bae
aleksei_abramov_2 <CQMZ4zCJWWpDYqz>
2022-12-13 20:01:08 +0600

    22.18 adding ThreadPoolExecutor

--- Performance/CarNumberGenerator/src/Loader.java
+++ Performance/CarNumberGenerator/src/Loader.java
@@ -1,17 +1,33 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class Loader {
 
     public static void main(String[] args) throws Exception {
         long start = System.currentTimeMillis();
-
-
-
-        for (int regionCode = 1; regionCode < 199; regionCode++) {
-            NumberThread numberThread = new NumberThread(regionCode);
-            numberThread.start();
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(12);
+        for (int regionCode = 1; regionCode <= 199; regionCode++) {
+            executor.execute(new NumbersWriter(regionCode));
         }
-        System.out.println((System.currentTimeMillis() - start) + " ms");
+        executor.shutdown();
+        while (!executor.isTerminated()) {
+        }
+        System.out.println(System.currentTimeMillis() - start);
     }
 
+    private static String padNumber(int number, int numberLength) {
+        String zero = "0";
+        String numberStr = Integer.toString(number);
+        int padSize = numberLength - numberStr.length();
+
+        for (int i = 0; i < padSize; i++) {
+            numberStr = zero.concat(numberStr);
+        }
+
+        return numberStr;
+    }
 
 }
